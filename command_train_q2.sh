f29cd52b270a04acdd9d76a37e0398d61120e1c2
Ehsan Hosseiniasl <ehsan@ip-10-20-8-10.us-west-2.compute.internal>
2019-03-12 09:44:28 +0000

    update sh

---
+++ command_train_q2.sh
@@ -0,0 +1,4 @@
+
+MODEL=question2_lstm_${2}_${3}_${4}
+
+CUDA_VISIBLE_DEVICES=$1 python main.py --embedding-size $2 --hidden-size $3 --layer $4 --classes $5 --cuda --data $6 --rnn LSTM --model $MODEL --multi_label 
