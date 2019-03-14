c7900dcf0165d4a627adeb233c21554ed2ed4f96
EllieNo <elno13@yahoo.com>
2019-03-14 01:46:43 -0700

    Update train_bert_q1.sh

--- Bert/train_bert_q1.sh
+++ Bert/train_bert_q1.sh
@@ -1,3 +1,7 @@
 
 
+CUDA_VISIBLE_DEVICES=$1 python run_classifier.py --data_dir $2 --do_train --do_eval --bert_model $3 --num_train_epochs $4 --task_name cola --output_dir checkpoints/sent_classification
+
+
+
 CUDA_VISIBLE_DEVICES=$1 python examples/run_classifier.py --data_dir $2 --do_train --do_eval --bert_model $3 --num_train_epochs $4 --task_name cola --output_dir checkpoints/sent_classification
