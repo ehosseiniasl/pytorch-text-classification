e9a68cf4ccb8a2632dac86c61a6c0643e3499181
Ehsan Hosseiniasl <ehsan@ip-10-20-9-87.us-west-2.compute.internal>
2019-03-14 09:07:22 +0000

    fix

--- Bert/run_classifier.py
+++ Bert/run_classifier.py
@@ -180,7 +180,7 @@ class ColaProcessor(DataProcessor):
     def get_dev_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+            self._read_tsv(os.path.join(data_dir, "val.tsv")), "dev")
 
     def get_labels(self):
         """See base class."""
