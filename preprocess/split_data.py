37b3ca40165ccf36ea17e1b25df038886c4214f6
Ehsan Hosseiniasl <ehsan@ip-10-20-9-87.us-west-2.compute.internal>
2019-03-23 21:01:45 +0000

    update cleanning

--- preprocess/split_data.py
+++ preprocess/split_data.py
@@ -5,6 +5,38 @@ import random
 import ipdb
 import csv
 import re
+import nltk
+from nltk.stem import PorterStemmer
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+stop_words = stopwords.words('english')
+porter = PorterStemmer()
+
+def tokenizer_porter(text):
+    return ' '.join([porter.stem(word) for word in text.split()])
+
+def remove_stop_words(text):
+    return ' '.join([word for word in text.split() if word not in stop_words])
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "can not ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r"\'scuse", " excuse ", text)
+    text = re.sub('\W', ' ', text)
+    text = re.sub('\s+', ' ', text)
+    text = text.strip(' ')
+    text = tokenizer_porter(text)
+    text = remove_stop_words(text)
+    return text
 
 filename = sys.argv[1]
 savepath = os.path.dirname(filename) 
@@ -25,7 +57,8 @@ with open(os.path.join(savepath, 'train.csv'), 'wt') as f:
     for l in tr:
         if 'S0' in l[1]:
             continue
-        txt = re.sub(r'[^\w\s]','',l[0]).lower()
+        #txt = re.sub(r'[^\w\s]','',l[0]).lower()
+        txt = clean_text(l[0])
         if txt in ['', ' ']:
             continue
         f.write('{},{}\n'.format(txt,l[-1]))
@@ -36,7 +69,8 @@ with open(os.path.join(savepath, 'val.csv'), 'wt') as f:
     for l in vl:
         if 'S0' in l[1]:
             continue
-        txt = re.sub(r'[^\w\s]','',l[0]).lower()
+        #txt = re.sub(r'[^\w\s]','',l[0]).lower()
+        txt = clean_text(l[0])
         if txt in ['', ' ']:
             continue
         f.write('{},{}\n'.format(txt,l[-1]))
@@ -47,7 +81,8 @@ with open(os.path.join(savepath, 'test.csv'), 'wt') as f:
     for l in ts:
         if 'S0' in l[1]:
             continue
-        txt = re.sub(r'[^\w\s]','',l[0]).lower()
+        #txt = re.sub(r'[^\w\s]','',l[0]).lower()
+        txt = clean_text(l[0])
         if txt in ['', ' ']:
             continue
         f.write('{},{}\n'.format(txt,l[-1]))
