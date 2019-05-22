65cc7c200cfdbc6fac0023093d3852b47d2c161f
Ehsan Hosseini-Asl <ehsan.hosseiniasl@gmail.com>
2019-05-22 14:40:22 -0700

    update

--- preprocess/split_query_data.py
+++ preprocess/split_query_data.py
@@ -11,6 +11,7 @@ import nltk
 from nltk.stem import PorterStemmer
 nltk.download('stopwords')
 from nltk.corpus import stopwords
+import ipdb
 
 stop_words = stopwords.words('english')
 porter = PorterStemmer()
@@ -40,6 +41,26 @@ def clean_text(text):
     text = remove_stop_words(text)
     return text
 
+
+def clean_text_v2(text):
+    text = text.lower()
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "can not ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r"\'scuse", " excuse ", text)
+    text = re.sub('\W', ' ', text)
+    text = re.sub('\s+', ' ', text)
+    text = text.strip(' ')
+    #text = tokenizer_porter(text)
+    text = remove_stop_words(text)
+    return text
+
 def clean_text_v1(text):
     text = text.lower()
     text.replace(',', ' ')
@@ -51,60 +72,8 @@ savepath = os.path.dirname(filename)
 
 df = pandas.read_csv(open(filename, 'rt', encoding='utf-8'))
 
+
 LABEL = 'Answer.information'
-
-#if 'Q2' in filename:
-#    LABEL = 'S0-Q2-device'
-#elif 'Q1' in filename:
-#    if 'yesno' in filename:
-#        LABEL = 'S0-Q1-yesno'
-#    elif 'format' in filename:
-#        LABEL = 'S0-Q1-format'
-#    elif 'recom' in filename:
-#        LABEL = 'S0-Q1-recom'
-#else:
-#    LABEL = 'S0-Q3-action'
-#
-#if 'Q2' in LABEL:
-#    label_set = [
-#            'device_speaker_screen',
-#            'device_camera',
-#            'device__wearable',
-#            'device_tablet',
-#            'device_kitchen',
-#            'device_speaker_no_screen']
-#elif 'Q3' in LABEL:
-#    label_set = [
-#            'assist_action_ad',
-#            'assist_action_image',
-#            'assist_action_sub',
-#            'assist_action_search',
-#            'assist_action_video',
-#            'assist_action_activate',
-#            'assist_action_clarify'
-#            ]
-#elif 'Q1' in LABEL:
-#    if 'format' in LABEL:
-#        label_set = [
-#                'Audio',
-#                'Image',
-#                'Video',
-#                'Text',
-#                'other'
-#                ]
-#
-#label_set = []
-#labels = df[LABEL]
-#for l in labels:
-#    if not isinstance(l, str):
-#        continue
-#    tmp = l.strip().split('$')
-#    for t in tmp:
-#        if t not in label_set:
-#            label_set.append(t)
-#label_set.remove('')
-#label_set.sort()
-
 INPUT = ['Input.Step', 'Answer.query', 'Answer.question']
 
 all_data = []
@@ -116,12 +85,17 @@ data_cleaned = []
 for txt1, txt2, txt3, l in all_data:
     if not isinstance(l, str):
         continue
-    txt_cleaned1 = clean_text(txt1)
-    txt_cleaned2 = clean_text(txt2)
-    txt_cleaned3 = clean_text(txt3)
-    l_cleaned = clean_text(l)
+    
+    try:
+        txt_cleaned1 = clean_text_v2(txt1)
+        txt_cleaned2 = clean_text_v2(txt2)
+        txt_cleaned3 = clean_text_v2(txt3)
+    except:
+        continue
+    l_cleaned = clean_text_v2(l)
 
-    data_cleaned.append((txt_cleaned1, txt_cleaned2, txt_cleaned2, l_cleaned))
+
+    data_cleaned.append((txt_cleaned1, txt_cleaned2, txt_cleaned3, l_cleaned))
 
 read = data_cleaned
 
@@ -143,6 +117,11 @@ with open(os.path.join(savepath, 'train_step_query.csv'), 'wt') as f:
     for l in tr:
         f.write('{},{}\n'.format(l[0] + ' ' + l[1], l[-1]))
 
+with open(os.path.join(savepath, 'train_step_question.csv'), 'wt') as f:
+    #f.write('{},{},{}\n'.format('step', 'query', 'information'))
+    f.write('{},{}\n'.format('step_question', 'information'))
+    for l in tr:
+        f.write('{},{}\n'.format(l[0] + ' ' + l[2], l[-1]))
 
 with open(os.path.join(savepath, 'train_step_query_question.csv'), 'wt') as f:
     f.write('{},{}\n'.format('step_query_question', 'information'))
@@ -160,6 +139,10 @@ with open(os.path.join(savepath, 'val_step_query.csv'), 'wt') as f:
     for l in vl:
         f.write('{},{}\n'.format(l[0] + ' ' + l[1], l[-1]))
 
+with open(os.path.join(savepath, 'val_step_question.csv'), 'wt') as f:
+    f.write('{},{}\n'.format('step_question', 'information'))
+    for l in vl:
+        f.write('{},{}\n'.format(l[0] + ' ' + l[2], l[-1]))
 
 with open(os.path.join(savepath, 'val_step_query_question.csv'), 'wt') as f:
     f.write('{},{}\n'.format('step_query_question', 'information'))
@@ -178,6 +161,10 @@ with open(os.path.join(savepath, 'test_step_query.csv'), 'wt') as f:
     for l in ts:
         f.write('{},{}\n'.format(l[0] + ' ' + l[1], l[-1]))
 
+with open(os.path.join(savepath, 'test_step_question.csv'), 'wt') as f:
+    f.write('{},{}\n'.format('step_question', 'information'))
+    for l in ts:
+        f.write('{},{}\n'.format(l[0] + ' ' + l[2], l[-1]))
 
 with open(os.path.join(savepath, 'test_step_query_question.csv'), 'wt') as f:
     f.write('{},{}\n'.format('step_query_question', 'information'))
@@ -185,3 +172,38 @@ with open(os.path.join(savepath, 'test_step_query_question.csv'), 'wt') as f:
         f.write('{},{}\n'.format(l[0] + ' ' + l[1] + ' ' +l[2], l[-1]))
 
 
+
+
+with open(os.path.join(savepath, 'test_step_text.csv'), 'wt') as f:
+    for l in ts:
+        f.write('{}\n'.format(l[0]))
+
+with open(os.path.join(savepath, 'test_step_query_text.csv'), 'wt') as f:
+    for l in ts:
+        f.write('{}\n'.format(l[0] + ' ' + l[1]))
+
+with open(os.path.join(savepath, 'test_step_question_text.csv'), 'wt') as f:
+    for l in ts:
+        f.write('{}\n'.format(l[0] + ' ' + l[2]))
+
+with open(os.path.join(savepath, 'test_step_query_question_text.csv'), 'wt') as f:
+    for l in ts:
+        f.write('{}\n'.format(l[0] + ' ' + l[1] + ' ' +l[2]))
+
+
+
+with open(os.path.join(savepath, 'test_step_target.csv'), 'wt') as f:
+    for l in ts:
+        f.write('{}\n'.format(l[-1]))
+
+with open(os.path.join(savepath, 'test_step_query_target.csv'), 'wt') as f:
+    for l in ts:
+        f.write('{}\n'.format(l[-1]))
+
+with open(os.path.join(savepath, 'test_step_question_target.csv'), 'wt') as f:
+    for l in ts:
+        f.write('{}\n'.format(l[-1]))
+
+with open(os.path.join(savepath, 'test_step_query_question_target.csv'), 'wt') as f:
+    for l in ts:
+        f.write('{}\n'.format(l[-1]))
